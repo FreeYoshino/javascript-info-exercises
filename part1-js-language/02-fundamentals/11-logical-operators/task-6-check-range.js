5e237bd03b8c1188088156f01d20fcec1f7be586
FreeYoshino <abc0940415@gmail.com>
2025-11-23 19:08:01 +0800

    Complete Check the range between task

---
+++ part1-js-language/02-fundamentals/11-logical-operators/task-6-check-range.js
@@ -0,0 +1,10 @@
+/* 
+  Write an if condition to check that age is between 14 and 90 inclusively.
+  “Inclusively” means that age can reach the edges 14 or 90.
+*/
+
+const age = 60; // change this value to test
+
+if (age >= 14 && age <= 90) {
+  console.log("Age is between 14 and 90 inclusively.");
+}
