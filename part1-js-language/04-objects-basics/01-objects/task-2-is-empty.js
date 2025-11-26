b56fe5159a7e7cfbf80f592994a3a502bb7dbfc6
FreeYoshino <abc0940415@gmail.com>
2025-11-26 21:25:56 +0800

    feat(p1-04-01-objects): complete is empty task

---
+++ part1-js-language/04-objects-basics/01-objects/task-2-is-empty.js
@@ -0,0 +1,21 @@
+/* 
+  Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
+  Should work like that:
+    let schedule = {};
+    alert( isEmpty(schedule) ); // true=
+    schedule["8:30"] = "get up";
+    alert( isEmpty(schedule) ); // false
+*/
+
+const isEmpty = (obj) => {
+  for (let key in obj) {
+    // 如果能遍歷這個objcet 代表不為空
+    return false;
+  }
+  return true;
+};
+
+let schedule = {};
+console.log(isEmpty(schedule));
+schedule["8:30"] = "get up";
+console.log(isEmpty(schedule));
