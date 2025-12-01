003984e67062bb64f7aad033f78682334e769071
FreeYoshino <abc0940415@gmail.com>
2025-12-01 14:49:11 +0800

    feat(p1-05-02-numbers): complete sum numbers task

---
+++ part1-js-language/05-data-types/02-numbers/task-1-sum-numbers.js
@@ -0,0 +1,17 @@
+/* 
+  Create a script that prompts the visitor to enter two numbers and then shows their sum.
+*/
+
+const readline = require("node:readline/promises");
+const { stdin: input, stdout: output } = require("node:process");
+
+const main = async () => {
+  const rl = readline.createInterface({ input, output });
+  const num1 = await rl.question("Enter the first number: ");
+  const num2 = await rl.question("Enter the second number: ");
+  const sum = Number(num1) + Number(num2);
+  console.log(`Sum of ${num1} and ${num2} is ${sum}.`);
+  rl.close();
+};
+
+main();
