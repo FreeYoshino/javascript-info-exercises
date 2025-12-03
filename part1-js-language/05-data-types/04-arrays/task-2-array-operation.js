2c547d5915077ff911e3d56fdfedc76293401191
FreeYoshino <abc0940415@gmail.com>
2025-12-03 16:06:52 +0800

    feat(p1-05-04-arrays): complete array operation task

---
+++ part1-js-language/05-data-types/04-arrays/task-2-array-operation.js
@@ -0,0 +1,14 @@
+/* 
+  Let’s try 5 array operations.
+  - Create an array styles with items “Jazz” and “Blues”.
+  - Append “Rock-n-Roll” to the end.
+  - Replace the value in the middle with “Classics”. Your code for finding the middle value should work for any arrays with odd length.
+  - Strip off the first value of the array and show it.
+  - Prepend Rap and Reggae to the array.
+*/
+
+const styles = ["Jazz", "Blues"];
+styles.push("Rock-n-Roll");
+styles[Math.floor(styles.length / 2)] = "Classics";
+console.log(styles.shift());
+styles.unshift("Rap", "Reggae");
