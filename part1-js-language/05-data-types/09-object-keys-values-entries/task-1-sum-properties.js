b69a93512591705dbfee70b59b5655b31b7394c9
FreeYoshino <abc0940415@gmail.com>
2025-12-11 19:04:08 +0800

    feat(p1-05-09-object-keys-values-entries): complete sum properties task

---
+++ part1-js-language/05-data-types/09-object-keys-values-entries/task-1-sum-properties.js
@@ -0,0 +1,29 @@
+/* 
+  There is a salaries object with arbitrary number of salaries.
+  Write the function sumSalaries(salaries) that returns the sum of all salaries using Object.values and the for..of loop.
+  If salaries is empty, then the result must be 0.
+
+  For instance:
+    let salaries = {
+      "John": 100,
+      "Pete": 300,
+      "Mary": 250
+    };
+    alert( sumSalaries(salaries) ); // 650
+*/
+
+const sumSalaries = (salaries) => {
+  let sum = 0;
+  for (let salary of Object.values(salaries)) {
+    sum += salary;
+  }
+  return sum;
+}
+
+// Example usage:
+let salaries = {
+  "John": 100,
+  "Pete": 300,
+  "Mary": 250
+};
+console.log(sumSalaries(salaries)); // 650
